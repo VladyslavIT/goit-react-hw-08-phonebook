483af497e229b4c2dcf442b27a1fc0e0868c6846
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-11-01 16:28:42 +0200

    fix localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,7 +18,7 @@ import {
 
 const App = () => {
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) ?? '';
+    return JSON.parse(localStorage.getItem('contacts')) ?? [];
   });
   const [filter, setFilter] = useState('');
 
