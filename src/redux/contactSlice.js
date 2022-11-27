774d48be5f2aeaf16901965d7658a5d5c5a2b58b
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-11-27 18:56:06 +0200

    fix

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,34 +1,37 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://638365b36e6c83b7a9927d4c.mockapi.io/api' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://638365b36e6c83b7a9927d4c.mockapi.io/api',
+  }),
   tagTypes: ['contact'],
-  endpoints: (builder) => ({
+  endpoints: builder => ({
     getContacts: builder.query({
       query: () => `/contacts`,
-      providesTags: ['contact']
+      providesTags: ['contact'],
     }),
     addContact: builder.mutation({
       query: body => ({
         url: `/contacts`,
         method: 'POST',
-        body
+        body,
       }),
       invalidatesTags: ['contact'],
     }),
-     
+
     deleteContact: builder.mutation({
       query: id => ({
         url: `/contacts/${id}`,
-        method: 'DELETE'
+        method: 'DELETE',
       }),
       invalidatesTags: ['contact'],
-    })
+    }),
   }),
 });
 
-
-
-export const { useGetContactsQuery, useAddContactMutation, useDeleteContactMutation } = contactsApi;
+export const {
+  useGetContactsQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+} = contactsApi;
