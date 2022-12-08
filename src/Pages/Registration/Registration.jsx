abdfdd238d2ffd1a75a6799e9a87cef60645bf58
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-12-09 00:25:34 +0200

    fix

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -123,7 +123,7 @@ export default function Registration() {
                   name="email"
                   autoComplete="email"
                   helperText='Email must contain @gmail.com'
-                  inputProps={{ className: 'input', pattern: "^([a-z0-9_-]+/.)*[a-z0-9_-]+@[a-z0-9_-]+(/.[a-z0-9_-]+)*/.[a-z]{2,6}$" }}
+                  inputProps={{ className: 'input', pattern: "^[a-z0-9._%+-]+@[a-z0-9.-]+$" }}
                 />
               </Grid>
 
